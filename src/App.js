2771c76abe268e71dd8fbc66fa3b35800914fdd2
PrantikC <prantik.chowdhury.25696@gmail.com>
2023-06-03 19:56:22 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./app.css";
+import Home from "./components/pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./components/pages/userList/UserList";
+import User from "./components/pages/user/User";
+import NewUser from "./components/pages/newUser/NewUser";
+import ProductList from "./components/pages/productList/ProductList";
+import Product from "./components/pages/product/Product";
+import NewProduct from "./components/pages/newProduct/NewProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
+          <Route path="/newUser" element={<NewUser/>}></Route>
+          <Route path="/products" element={<ProductList/>}></Route>
+          <Route path="/product/:productsId" element={<Product />} />
+          <Route path="/newProduct" element={<NewProduct />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
